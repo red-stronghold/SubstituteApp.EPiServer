9f6259398d3e34e275f05fa0a909494f40021b4f
Johan Redborg <johan.redborg@gmail.com>
2012-05-14 15:33:47 +0200

    initial checkin of solution

---
+++ SubstituteApp.EPiServer/install.ps1
@@ -0,0 +1,49 @@
+﻿# First some common params, delivered by the nuget package installer
+param($installPath, $toolsPath, $package, $project)
+
+# Get project path
+$path = [System.IO.Path]
+$projectpath = $path::GetDirectoryName($project.FileName)
+
+# Copy content from old episerver.config -> episerver.config.config
+$episerverconfig = $path::Combine($projectpath, "episerver.config")
+$episerverconfigconfig = $path::Combine($projectpath, "episerver.config.config")
+copy $episerverconfig $episerverconfigconfig
+
+# Copy content from old EPiServerFramework.config -> EPiServerFramework.config.config
+$episerverframeworkconfig = $path::Combine($projectpath, "EPiServerFramework.config")
+$episerverframeworkconfigconfig = $path::Combine($projectpath, "EPiServerFramework.config.config")
+copy $episerverframeworkconfig $episerverframeworkconfigconfig
+
+# Copy content from old connectionStrings.config -> connectionStrings.config.config
+$connectionstringsconfig = $path::Combine($projectpath, "connectionStrings.config")
+$connectionstringsconfigconfig = $path::Combine($projectpath, "connectionStrings.config.config")
+copy $connectionstringsconfig $connectionstringsconfigconfig
+
+# Get the build project of type [Microsoft.Build.Evaluation.Project]
+$buildProject = Get-Project $project.ProjectName | % {
+            $path = $_.FullName
+            @([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($path))[0]
+        }
+
+# Find the Content node for "episerver.config.substitute.xml"
+$episerverconfigsubstitutenode = $buildProject.Xml.ItemGroups | foreach {$_.Items} | Where-Object {$_.Include -match "episerver.config.substitute.xml"}
+
+# Add a dependency to "episerver.config.config" for "episerver.config.substitute.xml"
+$episerverconfigsubstitutenode.AddMetaData("DependentUpon", "episerver.config.config")
+
+# Find the Content node for "EPiServerFramework.config.substitute.xml"
+$episerverframeworkconfigsubstitutenode = $buildProject.Xml.ItemGroups | foreach {$_.Items} | Where-Object {$_.Include -match "EPiServerFramework.config.substitute.xml"}
+
+# Add a dependency to "EPiServerFramework.config.config" for "EPiServerFramework.config.substitute.xml"
+$episerverframeworkconfigsubstitutenode.AddMetaData("DependentUpon", "EPiServerFramework.config.config")
+
+# Find the Content node for "connectionStrings.config.substitute.xml"
+$connectionstringsconfigsubstitutenode = $buildProject.Xml.ItemGroups | foreach {$_.Items} | Where-Object {$_.Include -match "connectionStrings.config.substitute.xml"}
+
+# Add a dependency to "connectionStrings.config.config" for "connectionStrings.config.substitute.xml"
+$connectionstringsconfigsubstitutenode.AddMetaData("DependentUpon", "connectionStrings.config.config")
+
+
+# Persists the changes in project
+$project.Save() 
---
+++ build.boo
@@ -0,0 +1,48 @@
+import System.IO
+
+solution_file = "SubstituteApp.EPiServer.sln"
+project_dir = "SubstituteApp.EPiServer"
+nuspecfile = "SubstituteApp.EPiServer.nuspec"
+build_folder  = "build/"
+nuget_folder  = "nuget/"
+build_config  = env('build.config')
+build_output  = env('build.output')
+
+target default, (compile):
+   pass
+   
+target artifact, (compile, copy_files):
+   pass
+   
+target nuget, (compile, copy_files, nuget_push):
+   pass
+
+target compile:
+   msbuild(file: solution_file, configuration: build_config, version: "4")
+
+target nuget_pack:
+    rmdir(nuget_folder)
+    mkdir(nuget_folder)
+
+    exec("Libraries/Phantom/lib/nuget/NuGet.exe", "pack ${nuspecfile} /o ${nuget_folder}")
+
+target nuget_push, (nuget_pack):
+    exec("Libraries/Phantom/lib/nuget/NuGet.exe", "push ${nuget_folder}SubstituteApp.EPiServer.1.0.9.nupkg")
+
+
+target prepare_folders:
+   exec("attrib", "-R ${project_dir}/** /S /D")
+   rmdir(build_folder)
+   mkdir(build_folder)
+
+desc "Copy files"
+target copy_files, (prepare_folders):
+  print "Copying files to package"
+
+  with FileList(project_dir):
+    .Include("*.config.config")
+    .Include("*.xml")
+    .Include("*.ps1")
+    .Include("*.transform")
+    .ForEach def(file):
+      file.CopyToDirectory(build_folder)
